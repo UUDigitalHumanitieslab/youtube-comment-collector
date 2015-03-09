7ca9cac4a477b740aa67d96272e7f38b925ccb01
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-03-09 12:00:40 +0100

    Complete rewrite, added configuration file

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # youtube-comment-collector
 The script *youtube.py* collects comments and metadata from [YouTube](https://www.youtube.com) 
 videos using the (now deprecated) [V2 API](https://developers.google.com/youtube/2.0/developers_guide_protocol). 
-The script uses the [feedparser package](https://pythonhosted.org/feedparser/).
-Run the script with `python youtube.py`. 
+The script uses the [feedparser package](https://pythonhosted.org/feedparser/) for parsing the YouTube feeds, 
+and the [requests](http://docs.python-requests.org/) library for parsing the Google Plus JSON responses. 
+Run the script with `python youtube.py` after you have set your API key in `example.cfg`. 
--- youtube.py
+++ youtube.py
@@ -1,34 +1,96 @@
 import feedparser
+import requests
 import csv
 import time
+import ConfigParser
 
-VIDEO_ID = 'vnKZ4pdSU-s'
+
+def main():
+	config = ConfigParser.RawConfigParser()
+	config.read('example.cfg')
+
+	with open('comments.csv', 'wb') as csv_file:
+		# write heading
+		csv_file.write(u'\uFEFF'.encode('utf-8')) # the UTF-8 BOM to hint Excel we are using that...
+		csv_writer = csv.writer(csv_file, dialect='excel', delimiter=';', quoting=csv.QUOTE_MINIMAL)
+		heading = ['username', 'firstname', 'lastname', 'location', \
+			'birthday', 'gender', 'aboutme', 'organizations', 'tagline', \
+			'occupation', 'skills', 'statistics', 'replycount', 'activity_id', 'published', 'comment']
+		csv_writer.writerow(heading)
+
+		# get first feed
+		url = 'https://gdata.youtube.com/feeds/api/videos/' + config.get('youtube', 'video_id') + '/comments'
+		r = requests.get(url, params={'v': '2.1', 'orderby': 'published', 'max-results': '50'})
+
+		# parse first feed
+		i = 1
+		print 'Now parsing feed ' + str(i) + '...'
+		feed = feedparser.parse(r.url)
+		csv_writer.writerows(dump_comments(feed, config))
+
+		# get new comments while there's a next url
+		next_url = get_next_url(feed)
+		while next_url: 
+			i += 1
+			print 'Now parsing feed ' + str(i) + '...'
+			feed = get_next_feed(next_url)
+			csv_writer.writerows(dump_comments(feed, config))
+			next_url = get_next_url(feed)
 
 
-def dump_comments(feed):
+def get_next_feed(next_url):
+	"""Try a few times to retrieve the feed, wait a few seconds on error""" 
+	for attempt in range(5):
+		feed = feedparser.parse(next_url)
+		if feed.status == 403: 
+			sleep = 30 * (attempt + 1)
+			print 'Waiting ' + str(sleep) + ' seconds before retrying'
+			time.sleep(sleep)
+		else: 
+			return feed
+	else:
+		raise ValueError("Feed unreachable")
+
+
+def get_next_url(feed):
+	"""Retrieve the next URL""" 
+	next_url = ''
+	for l in feed.feed.links:
+		if l.rel == 'next':
+			next_url = l.href
+	return next_url
+
+
+def dump_comments(feed, config):
 	comments = list()
 	for entry in feed.entries:
 		body = entry.content[0].value.replace('\n', ' ')
-		
+
 		author_feed = feedparser.parse(entry.author_detail.href)
-		firstname = get_author_attr(author_feed, 'yt_firstname')
-		lastname = get_author_attr(author_feed, 'yt_lastname')
+		google_id = get_author_attr(author_feed, 'yt_googleplususerid')
+		r = requests.get(config.get('googleplus', 'url') + google_id, 
+			params={'key' : config.get('googleplus', 'api_key')})
+		j = r.json()
+
+		firstname = j.get('name', {}).get('givenName', '')
+		lastname = j.get('name', {}).get('familyName', '')
 		location = get_author_attr(author_feed, 'yt_location')
-		age = get_author_attr(author_feed, 'yt_age')
-		gender = get_author_attr(author_feed, 'yt_gender')
-		books = get_author_attr(author_feed, 'yt_books')
-		aboutme = get_author_attr(author_feed, 'yt_aboutme')
-		company = get_author_attr(author_feed, 'yt_company')
-		hobbies = get_author_attr(author_feed, 'yt_hobbies')
-		hometown = get_author_attr(author_feed, 'yt_hometown')
-		occupation = get_author_attr(author_feed, 'yt_occupation')
-		school = get_author_attr(author_feed, 'yt_school')
+		birthday = j.get('birthday', '')
+		gender = j.get('gender', '')
+		aboutme = j.get('aboutMe', '')
+		organizations = str(j.get('organizations', ''))
+		tagline = j.get('tagline', '')
+		occupation = j.get('occupation', '')
+		skills = j.get('skills', '')
 		statistics = get_author_stats(author_feed)
+
+		reply_count = entry.yt_replycount
+		activity_id = entry.id.split(':')[-1]
 		
 		comment = [entry.author, firstname, lastname, location, \
-			age, gender, books, aboutme, company, hobbies, hometown, \
-			occupation, school, statistics, entry.published, body]
-		comment = [i.encode('utf-8') for i in comment] # todo: correct?
+			birthday, gender, aboutme, organizations, tagline, \
+			occupation, skills, statistics, reply_count, activity_id, entry.published, body]
+		comment = [i.encode('utf-8') for i in comment] 
 		comments.append(comment)
 	return comments
 
@@ -47,44 +109,7 @@ def get_author_stats(author_feed):
 	except (AttributeError, IndexError) as e:
 		value = ''
 	return str(value)
-	
 
-def main():
-	with open('comments.csv', 'wb') as csv_file:
-		csv_writer = csv.writer(csv_file, dialect='excel', delimiter=';', quoting=csv.QUOTE_MINIMAL)
-		heading = ['username', 'firstname', 'lastname', 'location', \
-			'age', 'gender', 'books', 'aboutme', 'company', 'hobbies', 'hometown', \
-			'occupation', 'school', 'statistics', 'published', 'comment']
-		csv_writer.writerow(heading)
-
-		print 'Now parsing feed 1...'
-		feed = feedparser.parse('https://gdata.youtube.com/feeds/api/videos/' + VIDEO_ID + '/comments')
-		feed, next_url = get_next_url(feed)
-		csv_writer.writerows(dump_comments(feed))
-
-		for i in range(25):
-			print 'Now parsing feed ' + str(i + 2) + '...'
-			feed = feedparser.parse(next_url)
-			feed, next_url = get_next_url(feed, next_url)
-			csv_writer.writerows(dump_comments(feed))
-			
-			
-def get_next_url(feed, current_url=''):
-# Try a few times to get the next feed URL, wait a few seconds on error
-	for attempt in range(5):
-		try: 
-			next_url = ''
-			for l in feed.feed.links:
-				if l.rel == 'next':
-					next_url = l.href
-		except AttributeError: 
-			print 'Waiting some time before retrying'
-			feed = feedparser.parse(current_url)
-			time.sleep(5) 
-		else: 
-			return feed, next_url
-	else:
-		return None
 
 if __name__ == "__main__":
 	main()
